e7c12e57a2580a9ab5826bc966c899e4e1573af4
Jin Su <sujinltenjoy@gmail.com>
2024-06-25 22:10:26 +0800

    Update foldseek_util.py

--- utils/foldseek_util.py
+++ utils/foldseek_util.py
@@ -13,26 +13,37 @@ def get_struc_seq(foldseek,
                   chains: list = None,
                   process_id: int = 0,
                   plddt_mask: bool = False,
-                  plddt_threshold: float = 70.) -> dict:
+                  plddt_threshold: float = 70.,
+                  foldseek_verbose: bool = False) -> dict:
     """
 
     Args:
         foldseek: Binary executable file of foldseek
+
         path: Path to pdb file
+
         chains: Chains to be extracted from pdb file. If None, all chains will be extracted.
+
         process_id: Process ID for temporary files. This is used for parallel processing.
+
         plddt_mask: If True, mask regions with plddt < plddt_threshold. plddt scores are from the pdb file.
+
         plddt_threshold: Threshold for plddt. If plddt is lower than this value, the structure will be masked.
+
+        foldseek_verbose: If True, foldseek will print verbose messages.
 
     Returns:
         seq_dict: A dict of structural seqs. The keys are chain IDs. The values are tuples of
         (seq, struc_seq, combined_seq).
     """
     assert os.path.exists(foldseek), f"Foldseek not found: {foldseek}"
-    assert os.path.exists(path), f"Pdb file not found: {path}"
+    assert os.path.exists(path), f"PDB file not found: {path}"
     
-    tmp_save_path = f"get_struc_seq_{process_id}.tsv"
-    cmd = f"{foldseek} structureto3didescriptor -v 0 --threads 1 --chain-name-mode 1 {path} {tmp_save_path}"
+    tmp_save_path = f"get_struc_seq_{process_id}_{time.time()}.tsv"
+    if foldseek_verbose:
+        cmd = f"{foldseek} structureto3didescriptor --threads 1 --chain-name-mode 1 {path} {tmp_save_path}"
+    else:
+        cmd = f"{foldseek} structureto3didescriptor -v 0 --threads 1 --chain-name-mode 1 {path} {tmp_save_path}"
     os.system(cmd)
     
     seq_dict = {}
