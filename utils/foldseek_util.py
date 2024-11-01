6ee3683ac1b8426d802b37e1cbb40996de486802
Jin Su <sujinltenjoy@gmail.com>
2024-11-01 10:59:45 +0800

    Update foldseek_util.py

--- utils/foldseek_util.py
+++ utils/foldseek_util.py
@@ -1,9 +1,13 @@
 import os
-import re
 import time
 import json
 import numpy as np
+import re
 import sys
+
+from Bio.PDB import PDBParser, MMCIFParser
+
+
 sys.path.append(".")
 
 
@@ -12,7 +16,7 @@ def get_struc_seq(foldseek,
                   path,
                   chains: list = None,
                   process_id: int = 0,
-                  plddt_mask: bool = False,
+                  plddt_mask: bool = True,
                   plddt_threshold: float = 70.,
                   foldseek_verbose: bool = False) -> dict:
     """
@@ -46,6 +50,11 @@ def get_struc_seq(foldseek,
         cmd = f"{foldseek} structureto3didescriptor -v 0 --threads 1 --chain-name-mode 1 {path} {tmp_save_path}"
     os.system(cmd)
     
+    # Check whether the structure is predicted by AlphaFold2
+    with open(path, "r") as r:
+        if "alphafold" not in r.read().lower():
+            plddt_mask = False
+    
     seq_dict = {}
     name = os.path.basename(path)
     with open(tmp_save_path, "r") as r:
@@ -54,14 +63,19 @@ def get_struc_seq(foldseek,
             
             # Mask low plddt
             if plddt_mask:
-                plddts = extract_plddt(path)
-                assert len(plddts) == len(struc_seq), f"Length mismatch: {len(plddts)} != {len(struc_seq)}"
+                try:
+                    plddts = extract_plddt(path)
+                    assert len(plddts) == len(struc_seq), f"Length mismatch: {len(plddts)} != {len(struc_seq)}"
+                    
+                    # Mask regions with plddt < threshold
+                    indices = np.where(plddts < plddt_threshold)[0]
+                    np_seq = np.array(list(struc_seq))
+                    np_seq[indices] = "#"
+                    struc_seq = "".join(np_seq)
                 
-                # Mask regions with plddt < threshold
-                indices = np.where(plddts < plddt_threshold)[0]
-                np_seq = np.array(list(struc_seq))
-                np_seq[indices] = "#"
-                struc_seq = "".join(np_seq)
+                except Exception as e:
+                    print(f"Error: {e}")
+                    print(f"Failed to mask plddt for {name}")
             
             name_chain = desc.split(" ")[0]
             chain = name_chain.replace(name, "").split("_")[-1]
@@ -85,28 +99,69 @@ def extract_plddt(pdb_path: str) -> np.ndarray:
     Returns:
         plddts: plddt scores.
     """
-    with open(pdb_path, "r") as r:
-        plddt_dict = {}
-        for line in r:
-            line = re.sub(' +', ' ', line).strip()
-            splits = line.split(" ")
-            
-            if splits[0] == "ATOM":
-                # If position < 1000
-                if len(splits[4]) == 1:
-                    pos = int(splits[5])
-                
-                # If position >= 1000, the blank will be removed, e.g. "A 999" -> "A1000"
-                # So the length of splits[4] is not 1
-                else:
-                    pos = int(splits[4][1:])
-                
-                plddt = float(splits[-2])
-                
-                if pos not in plddt_dict:
-                    plddt_dict[pos] = [plddt]
-                else:
-                    plddt_dict[pos].append(plddt)
+
+    # Initialize parser
+    if pdb_path.endswith(".cif"):
+        parser = MMCIFParser()
+    elif pdb_path.endswith(".pdb"):
+        parser = PDBParser()
+    else:
+        raise ValueError("Invalid file format for plddt extraction. Must be '.cif' or '.pdb'.")
     
-    plddts = np.array([np.mean(v) for v in plddt_dict.values()])
+    structure = parser.get_structure('protein', pdb_path)
+    model = structure[0]
+    chain = model["A"]
+
+    # Extract plddt scores
+    plddts = []
+    for residue in chain:
+        residue_plddts = []
+        for atom in residue:
+            plddt = atom.get_bfactor()
+            residue_plddts.append(plddt)
+        
+        plddts.append(np.mean(residue_plddts))
+
+    plddts = np.array(plddts)
     return plddts
+
+
+def transform_pdb_dir(foldseek: str, pdb_dir: str, seq_type: str, save_path: str):
+    """
+    Transform a directory of pdb files into a fasta file.
+    Args:
+        foldseek: Binary executable file of foldseek.
+        
+        pdb_dir: Directory of pdb files.
+        
+        seq_type: Type of sequence to be extracted. Must be "aa" or "foldseek"
+        
+        save_path: Path to save the fasta file.
+    """
+    assert os.path.exists(foldseek), f"Foldseek not found: {foldseek}"
+    assert seq_type in ["aa", "foldseek"], f"seq_type must be 'aa' or 'foldseek'!"
+    
+    tmp_save_path = f"get_struc_seq_{time.time()}.tsv"
+    cmd = f"{foldseek} structureto3didescriptor --chain-name-mode 1 {pdb_dir} {tmp_save_path}"
+    os.system(cmd)
+    
+    with open(tmp_save_path, "r") as r, open(save_path, "w") as w:
+        for line in r:
+            protein_id, aa_seq, foldseek_seq = line.strip().split("\t")[:3]
+            
+            if seq_type == "aa":
+                w.write(f">{protein_id}\n{aa_seq}\n")
+            else:
+                w.write(f">{protein_id}\n{foldseek_seq.lower()}\n")
+    
+    os.remove(tmp_save_path)
+    os.remove(tmp_save_path + ".dbtype")
+    
+
+if __name__ == '__main__':
+    foldseek = "/sujin/bin/foldseek"
+    # test_path = "/sujin/Datasets/PDB/all/6xtd.cif"
+    test_path = "/sujin/Datasets/FLIP/meltome/af2_structures/A0A061ACX4.pdb"
+    plddt_path = "/sujin/Datasets/FLIP/meltome/af2_plddts/A0A061ACX4.json"
+    res = get_struc_seq(foldseek, test_path, plddt_path=plddt_path, plddt_threshold=70.)
+    print(res["A"][1].lower())
