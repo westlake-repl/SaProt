35d12cc91882af3d5383b4f28b7552e1fdabce71
LTEnjoy <sujinltenjoy@gmail.com>
2025-03-12 13:58:51 +0800

    Update requirements

--- environment.sh
+++ environment.sh
@@ -1,12 +1,12 @@
 pip install torch==1.13.1+cu117 torchvision==0.14.1+cu117 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu117
 pip install pytorch-lightning==1.8.3
-pip install wandb
+pip install wandb==0.12.10
 pip install transformers==4.28.0
-pip install easydict
-pip install biopython
-pip install scipy
-pip install peft
-pip install lmdb
+pip install easydict==1.10
+pip install biopython==1.81
+pip install scipy==1.14.1
+pip install peft==0.10.0
+pip install lmdb==1.4.1
 pip install torchmetrics==0.9.3
-pip install pandas
-pip install fair-esm
+pip install pandas==2.1.1
+pip install fair-esm==2.0.0
